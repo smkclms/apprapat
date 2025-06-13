ecc9526f5924b0491c8caab23bac0309336539b4
smkclms <smkn1cilimus@gmail.com>
2025-06-13 20:01:01 +0700

    hapus metode get_all_logs pada log aktivitas

--- application/models/Log_aktivitas_model.php
+++ application/models/Log_aktivitas_model.php
@@ -7,13 +7,13 @@ class Log_aktivitas_model extends CI_Model {
         return $this->db->insert('log_aktivitas', $data);
     }
 
-    public function get_all_logs() {
-        $this->db->select('log_aktivitas.*, users.nama');
-        $this->db->from('log_aktivitas');
-        $this->db->join('users', 'log_aktivitas.id_user = users.id_user', 'left');
-        $this->db->order_by('waktu', 'DESC');
-        return $this->db->get()->result();
-    }
+    // public function get_all_logs() {
+    //     $this->db->select('log_aktivitas.*, users.nama');
+    //     $this->db->from('log_aktivitas');
+    //     $this->db->join('users', 'log_aktivitas.id_user = users.id_user', 'left');
+    //     $this->db->order_by('waktu', 'DESC');
+    //     return $this->db->get()->result();
+    // }
     public function get_recent_logs($limit = 5) {
     $this->db->select('log_aktivitas.*, users.nama');
     $this->db->from('log_aktivitas');
